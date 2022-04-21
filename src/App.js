e5b3dbaf2a5dddd5162ee24ca9259f241da48c13
Hooman Talakian <hooman.talakian@gmail.com>
2022-04-21 17:04:36 +0430

    rotation alert added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Instructions from "./components/Instructions";
 import Screen from "./components/Screen";
-// import "./normalize-8.0.1.css"
-import "./resetCSS-2.0.css"
-import "./App.css"
+import "./resetCSS-2.0.css";
+import "./App.css";
 import Background from "./assets/PC.jpg";
 
 function App() {
-
-
 	return (
-		<div className="app">
-            <img className="background" src={Background} alt="" />
-            <Screen className="screen"/>
-            <Instructions className="instruction"/>
+		<div>
+			<div className="app">
+				<img className="background" src={Background} alt="" />
+				<Screen className="screen" />
+				<Instructions className="instruction" />
+			</div>
+			<h2 className="alert">Please Rotate Your Device!</h2>
 		</div>
 	);
 }
